eb270d76acc769701e05a49d97586ed2a74ce593
Godstime Osarobo <nosarobo@gmail.com>
2023-04-26 12:25:49 +0100

    Added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
-const XLSX = require("xlsx");
+const XLSX = require('xlsx');
 
+let shouldDownload = false;
 let uniq = (a: Iterable<any> | null | undefined) => [...new Set(a)];
 let formatBusinesses = (businesses: (string[] | undefined)[]) => {
     const formattedBusinesses = businesses.map((business) => {
         if (business) {
             return [
-                business[0].replace(/\s+/g, " ").trim(),
-                business[1].replace(/\s+/g, " ").trim(),
-                business[2].replace(/\s+/g, " ").trim(),
-                business[3].replace(/\s+/g, " ").trim(),
-                business[4].replace(/\s+/g, " ").trim(),
+                business[0].replace(/\s+/g, ' ').trim(),
+                business[1].replace(/\s+/g, ' ').trim(),
+                business[2].replace(/\s+/g, ' ').trim(),
+                business[3].replace(/\s+/g, ' ').trim(),
+                business[4].replace(/\s+/g, ' ').trim(),
             ];
         } else {
-            return ["", "", "", "", ""];
+            return ['', '', '', '', ''];
         }
     });
     return formattedBusinesses;
@@ -24,32 +25,32 @@ let formatBusinesses = (businesses: (string[] | undefined)[]) => {
 let fetchData = async (
     setStatus: React.Dispatch<React.SetStateAction<string>>
 ) => {
-    console.log("Fetching data...");
-    setStatus("Fetching data...");
+    console.log('Fetching data...');
+    setStatus('Fetching data...');
 
     const businesses: Element[] = [];
-    const targetUrl = "https://www.sandyford.ie/business-directory/P";
+    const targetUrl = 'https://www.sandyford.ie/business-directory/P';
 
     for (let i = 0; i < 70; i++) {
-        let pagenumber = i * 8;
+        let pageNumber = i * 8;
         if (i === 0) {
-            pagenumber = 1;
+            pageNumber = 1;
         }
 
         console.log(
-            `Fetching page ${i} of ${70} from ${targetUrl + pagenumber}`
+            `Fetching page ${i} of ${70} from ${targetUrl + pageNumber}`
         );
-        setStatus(`Fetching page ${i} of ${70} from ${targetUrl + pagenumber}`);
+        setStatus(`Fetching page ${i} of ${70} from ${targetUrl + pageNumber}`);
 
         try {
-            const response = await fetch(targetUrl + pagenumber);
+            const response = await fetch(targetUrl + pageNumber);
             const data = await response.text();
             // Do something with the data
             const parser = new DOMParser();
-            const htmlDocument = parser.parseFromString(data, "text/html");
+            const htmlDocument = parser.parseFromString(data, 'text/html');
             const htmlElement = htmlDocument.documentElement;
 
-            const businessElement = htmlElement.querySelectorAll(".business");
+            const businessElement = htmlElement.querySelectorAll('.business');
             if (businessElement) {
                 businesses.push(...businessElement);
             } else {
@@ -57,12 +58,12 @@ let fetchData = async (
             }
         } catch (error) {
             // Handle any errors
-            console.error("Error:", error);
-            setStatus("Failed! Check console for error.");
+            console.error('Error:', error);
+            setStatus('Failed! Check console for error.');
         }
     }
-    console.log("Data fetched!");
-    setStatus("Data fetched!");
+    console.log('Data fetched!');
+    setStatus('Data fetched!');
 
     const uniqueBusinesses = uniq(businesses);
 
@@ -73,27 +74,27 @@ let downloadExcelFile = (
     businesses: any[],
     setStatus: React.Dispatch<React.SetStateAction<string>>
 ) => {
-    console.log("Creating the Excel file...");
-    setStatus("Creating the Excel file...");
+    console.log('Creating the Excel file...');
+    setStatus('Creating the Excel file...');
 
     const businessList = businesses.map((business: Element) => {
         if (business) {
-            const name = business.querySelector("h3 > a")?.textContent || "";
-            const address = business.querySelector("h3 + p")?.textContent || "";
+            const name = business.querySelector('h3 > a')?.textContent || '';
+            const address = business.querySelector('h3 + p')?.textContent || '';
             const phoneNumber =
-                business.querySelector(".grid-x + p > a")?.textContent || "";
+                business.querySelector('.grid-x + p > a')?.textContent || '';
             const email =
-                business.querySelector(".grid-x + p > a + a")?.textContent ||
-                "";
+                business.querySelector('.grid-x + p > a + a')?.textContent ||
+                '';
             const website =
-                business.querySelector(".grid-x + p + p")?.textContent || "";
+                business.querySelector('.grid-x + p + p')?.textContent || '';
             return [name, address, phoneNumber, email, website];
         }
-        return ["", "", "", "", ""];
+        return ['', '', '', '', ''];
     });
 
     const data = [
-        ["name", "address", "phone number", "email", "website"],
+        ['name', 'address', 'phone number', 'email', 'website'],
         // ["name", "address"],
         ...formatBusinesses(businessList),
     ];
@@ -109,102 +110,116 @@ let downloadExcelFile = (
         const ws = XLSX.utils.aoa_to_sheet(data);
 
         // Add the worksheet to the workbook
-        XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
 
         // Format the worksheet as a table
-        if (!ws["!ref"]) return null; // Sheet is empty (no data)
-        const range = XLSX.utils.decode_range(ws["!ref"]);
+        if (!ws['!ref']) return null; // Sheet is empty (no data)
+        const range = XLSX.utils.decode_range(ws['!ref']);
         for (let col = range.s.c; col <= range.e.c; col++) {
             const column = XLSX.utils.encode_col(col);
-            ws[column + "1"].s = { font: { bold: true } };
+            ws[column + '1'].s = { font: { bold: true } };
             for (let row = 2; row <= range.e.r; row++) {
                 ws[column + row].s = {
                     border: {
-                        bottom: { style: "thin", color: { auto: 1 } },
+                        bottom: { style: 'thin', color: { auto: 1 } },
                     },
                 };
             }
         }
-        ws["!autofilter"] = { ref: XLSX.utils.encode_range(range) };
-        ws["!cols"] = [{ width: 20 }, { width: 10 }, { width: 15 }]; // Set column widths
+        ws['!autofilter'] = { ref: XLSX.utils.encode_range(range) };
+        ws['!cols'] = [
+            { width: 30 },
+            { width: 50 },
+            { width: 20 },
+            { width: 30 },
+            { width: 50 },
+        ]; // Set column widths
 
-        console.log("Excel file created!");
-        setStatus("Excel file created!");
+        console.log('Excel file created!');
+        setStatus('Excel file created!');
 
         setTimeout(() => {
-            console.log("Downloading Excel file...");
-            setStatus("Downloading Excel file...");
+            console.log('Downloading Excel file...');
+            setStatus('Downloading Excel file...');
 
             // Write the workbook to a file
-            XLSX.writeFile(wb, "data.xlsx")
+            XLSX.writeFile(wb, 'data.xlsx');
 
-            console.log("Excel file downloaded!");
-            setStatus("Excel file downloaded!");
+            console.log('Excel file downloaded!');
+            setStatus('Excel file downloaded!');
         }, 2000);
     } catch (error) {
-        console.error("Error:", error);
-        setStatus("Failed! Check console for error.");
+        console.error('Error:', error);
+        setStatus('Failed! Check console for error.');
     }
 };
 
 let statusColor = (status: string) => {
     switch (status.toLowerCase()) {
-        case "fetching data...":
-            return "orange";
-        case "data fetched!":
-            return "green";
-        case "creating the excel file...":
-            return "orange";
-        case "excel file created!":
-            return "green";
-        case "downloading excel file...":
-            return "orange";
-        case "excel file downloaded!":
-            return "green";
-        case "failed! check console for error.":
-            return "red";
-        case "idle":
-            return "red";
+        case 'fetching data...':
+            return 'orange';
+        case 'data fetched!':
+            return 'green';
+        case 'creating the excel file...':
+            return 'orange';
+        case 'excel file created!':
+            return 'green';
+        case 'downloading excel file...':
+            return 'orange';
+        case 'excel file downloaded!':
+            return 'green';
+        case 'failed! check console for error.':
+            return 'red';
+        case 'idle':
+            return 'red';
         default:
-            return "orange";
+            return 'orange';
     }
 };
 
 function App() {
     const [businesses, setBusinesses] = useState<Element[]>([]);
-    const [status, setStatus] = useState<string>("Idle");
+    const [status, setStatus] = useState<string>('Idle');
     useEffect(() => {
-        if (businesses.length > 0) {
+        if (businesses.length > 0 && shouldDownload) {
             downloadExcelFile(businesses, setStatus);
+            shouldDownload = false;
         }
     }, [businesses]);
 
     const spanStyle = {
         color: statusColor(status),
-        fontSize: "20px",
-        fontWeight: "bold",
+        fontSize: '20px',
+        fontWeight: 'bold',
     };
 
     return (
         <div className="App">
-            <h1>Sandyford Business Directory Scraper</h1>
-            <button
-                onClick={async () => {
-                    setBusinesses(await fetchData(setStatus));
-                }}>
-                Download Excel Sheet of Sandyford Businesses
-            </button>
-            <h2>
-                Current Status: <span style={spanStyle}>{status}</span>
-            </h2>
+            <div className="container">
+                <h1>Sandyford Business Directory Scraper</h1>
+                <button
+                    onClick={async () => {
+                        shouldDownload = true;
+                        setBusinesses(await fetchData(setStatus));
+                    }}
+                >
+                    Download Excel Sheet of Sandyford Businesses
+                </button>
+                <h2>
+                    Current Status: <span style={spanStyle}>{status}</span>
+                </h2>
+            </div>
 
             <hr />
-            {businesses.map((business, index) => (
-                <div
-                    key={index}
-                    dangerouslySetInnerHTML={{ __html: business.innerHTML }}
-                />
-            ))}
+            <div className="card-container">
+                {businesses.map((business, index) => (
+                    <div
+                        className="card"
+                        key={index}
+                        dangerouslySetInnerHTML={{ __html: business.innerHTML }}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
